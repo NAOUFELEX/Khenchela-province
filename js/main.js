618892bc5eb1522ce77c558653800cba0abae4c1
naoufelpy <77999196+NAOUFELEX@users.noreply.github.com>
2022-01-15 22:14:14 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,107 @@
+$(document).ready(function(){
+    $(".hide").hide();
+    
+    
+    $(".marker-Cherchar").click(function(){
+      $(".hide").hide();
+      $(".checher-txt").fadeIn(300);
+    });
+    
+      $(".marker-Djellal").click(function(){
+         $(".hide").hide();
+      $(".djellal-txt").fadeIn(300);
+    });
+    
+      $(".marker-Eloueldja").click(function(){
+         $(".hide").hide();
+      $(".eloueldja-txt").fadeIn(300);
+    });
+    
+      $(".marker-Khirane").click(function(){
+         $(".hide").hide();
+      $(".khirane-txt").fadeIn(300);
+    });
+
+    $(".marker-Babar").click(function(){
+        $(".hide").hide();
+     $(".babar-txt").fadeIn(300);
+   });
+
+        $(".marker-Msara").click(function(){
+            $(".hide").hide();
+            $(".msara-txt").fadeIn(300);
+            });
+
+     $(".marker-Bouhmama").click(function(){
+         $(".hide").hide();
+         $(".bouhmama-txt").fadeIn(300);
+         });
+
+
+         $(".marker-Tamza").click(function(){
+            $(".hide").hide();
+            $(".tamza-txt").fadeIn(300);
+            });
+
+        $(".marker-Yabous").click(function(){
+             $(".hide").hide();
+             $(".yabous-txt").fadeIn(300);
+             });  
+         $(".marker-Ouledrechache").click(function(){
+            $(".hide").hide();
+            $(".ouledrechache-txt").fadeIn(300);
+            });
+
+        $(".marker-Elmahmal").click(function(){
+            $(".hide").hide();
+            $(".elmahmal-txt").fadeIn(300);
+            });
+
+        $(".marker-Aintouila").click(function(){
+            $(".hide").hide();
+             $(".aintouila-txt").fadeIn(300);
+             });                
+
+        $(".marker-Mtoussa").click(function(){
+            $(".hide").hide();
+            $(".mtoussa-txt").fadeIn(300);
+             });
+
+        $(".marker-Baghai").click(function(){
+            $(".hide").hide();
+            $(".baghai-txt").fadeIn(300);
+            });
+
+            $(".marker-Elhamma").click(function(){
+                $(".hide").hide();
+                $(".elhamma-txt").fadeIn(300);
+              });
+
+              $(".remila-txt").click(function(){
+                $(".hide").hide();
+                $(".russia-txt").fadeIn(300);
+              });
+
+              $(".marker-Taouziant").click(function(){
+                $(".hide").hide();
+                $(".taouziant-txt").fadeIn(300);
+              });
+
+              $(".marker-Ensigha").click(function(){
+                $(".hide").hide();
+                $(".ensigha-txt").fadeIn(300);
+              });
+
+              $(".marker-Khenchela").click(function(){
+                $(".hide").hide();
+                $(".khenchela-txt").fadeIn(300);
+              });
+
+              $(".marker-Kais").click(function(){
+                $(".hide").hide();
+                $(".Kais-txt").fadeIn(300);
+              });
+  });
+
+
+
